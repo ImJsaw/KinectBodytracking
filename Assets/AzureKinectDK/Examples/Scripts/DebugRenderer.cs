322f395958a4e0ee30c2202561c21feb4e92c349
賓 <bing5300@gmail.com>
2020-02-11 15:50:34 +0800

    now can transform skeleton data between class & byte[]

--- Assets/AzureKinectDK/Examples/Scripts/DebugRenderer.cs
+++ Assets/AzureKinectDK/Examples/Scripts/DebugRenderer.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using Microsoft.Azure.Kinect.Sensor;
 using Microsoft.Azure.Kinect.Sensor.BodyTracking;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 public class DebugRenderer : MonoBehaviour
 {
@@ -65,18 +67,35 @@ public class DebugRenderer : MonoBehaviour
                 renderer.material.mainTexture = tex;
             }
         }
-        //BODY
-        using (var frame = tracker.PopResult())
-        {
+        //split for force update
+        //updateBody();
+        selfUpdate();
+    }
+
+    void updateBody() {
+        using (var frame = tracker.PopResult()) {
             Debug.LogFormat("{0} bodies found.", frame.NumBodies);
-            if (frame.NumBodies > 0)
-            {
+            if (frame.NumBodies > 0) {
                 var bodyId = frame.GetBodyId(0);
                 Debug.LogFormat("bodyId={0}", bodyId);
                 //send skeleton
-                this.skeleton = frame.GetSkeleton(0);
-                for (var i = 0; i < (int)JointId.Count; i++)
-                {
+                //this.skeleton = frame.GetSkeleton(0);
+
+                //send from net
+                byte[] userDataBytes;
+                MemoryStream ms = new MemoryStream();
+                BinaryFormatter bf1 = new BinaryFormatter();
+                bf1.Serialize(ms, frame.GetSkeleton(0));
+                userDataBytes = ms.ToArray();
+
+                //get data from net
+                MemoryStream ms2 = new MemoryStream(userDataBytes);
+                BinaryFormatter bf2 = new BinaryFormatter();
+                ms.Position = 0;
+                object rawObj = bf1.Deserialize(ms);
+                this.skeleton = (Skeleton)rawObj;
+
+                for (var i = 0; i < (int)JointId.Count; i++) {
                     var joint = this.skeleton.Joints[i];
                     var pos = joint.Position;
                     var rot = joint.Orientation;
@@ -85,7 +104,30 @@ public class DebugRenderer : MonoBehaviour
                     var obj = debugObjects[i];
                     obj.transform.SetPositionAndRotation(v, r);
                 }
-                
+
+            }
+        }
+    }
+
+    void selfUpdate() {
+        using (var frame = tracker.PopResult()) {
+            Debug.LogFormat("{0} bodies found.", frame.NumBodies);
+            if (frame.NumBodies > 0) {
+                var bodyId = frame.GetBodyId(0);
+                Debug.LogFormat("bodyId={0}", bodyId);
+                //send skeleton
+               this.skeleton = frame.GetSkeleton(0);
+
+                for (var i = 0; i < (int)JointId.Count; i++) {
+                    var joint = this.skeleton.Joints[i];
+                    var pos = joint.Position;
+                    var rot = joint.Orientation;
+                    var v = new Vector3(pos[0], -pos[1], pos[2]) * 0.004f;
+                    var r = new Quaternion(rot[1], rot[2], rot[3], rot[0]);
+                    var obj = debugObjects[i];
+                    obj.transform.SetPositionAndRotation(v, r);
+                }
+
             }
         }
     }
