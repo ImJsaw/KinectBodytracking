aadc0411e520dd3368c5bab83b693cfa1701dd82
賓 <bing5300@gmail.com>
2020-09-26 16:04:56 +0800

    refactor General.cs & fix pick model problem

--- Assets/Scripts/General.cs
+++ Assets/Scripts/General.cs
@@ -6,8 +6,9 @@ using UnityEngine;
 public class General : MonoBehaviour {
 
     public ObsController obsController;
-    public IKModelController[] modelPrefab;
-    public IKModelController[] modelPrefabSelf;
+    public GameObject[] modelPrefab;
+    public GameObject[] modelPrefabSelf;
+    public string[] prefix;
     //public ModelController modelPrefab = null;
     private Dictionary<string, int> localPlayerUIDDict = new Dictionary<string, int>();
 
@@ -36,27 +37,25 @@ public class General : MonoBehaviour {
             obsModelPrefab.modelIndex = index;
         } else if (UID == MainMgr.inst.myUID() ) {
             //if self, generate no head model
-            IKModelController ikModelPrefab = new IKModelController();
+            GameObject model;
             if (MainMgr.inst.is_custom)
             {
                 GameObject customModel = MainMgr.inst.customModelList[0];
-                GameObject newer = Instantiate(customModel, new Vector3(0, 0, -6), Quaternion.identity);
-                newer.AddComponent<IKModelController>();
-                newer.AddComponent<RootMotion.FinalIK.IKauto>();
-                newer.AddComponent<Lock>();
-                newer.transform.localScale = new Vector3(100, 100, 100);
-                ikModelPrefab = newer.GetComponent<IKModelController>();
+                model = Instantiate(customModel, new Vector3(0, 0, -6), Quaternion.identity);
+                model.transform.localScale = new Vector3(100, 100, 100);
             }
             else
             {
-                ikModelPrefab = Instantiate(modelPrefabSelf[MainMgr.inst.modelType[index]], new Vector3(0, 0, -6), Quaternion.identity);
-                ikModelPrefab.gameObject.AddComponent<RootMotion.FinalIK.IKauto>();
-                ikModelPrefab.gameObject.AddComponent<Lock>();
+                model = Instantiate(modelPrefabSelf[MainMgr.inst.modelType[index]], new Vector3(0, 0, -6), Quaternion.identity);
             }
-            ikModelPrefab.modelIndex = index;
+            model.AddComponent<IKModelController>();
+            model.AddComponent<RootMotion.FinalIK.IKauto>();
+            model.AddComponent<Lock>();
+            model.GetComponent<IKModelController>().prefix = prefix[index];
+            model.GetComponent<IKModelController>().modelIndex = index;
         } else {
-            IKModelController ikModelPrefab = Instantiate(modelPrefab[MainMgr.inst.modelType[index]], new Vector3(0, 0, -6), Quaternion.identity);
-            ikModelPrefab.modelIndex = index;
+            GameObject model = Instantiate(modelPrefab[MainMgr.inst.modelType[index]], new Vector3(0, 0, -6), Quaternion.identity);
+            model.GetComponent<IKModelController>().modelIndex = index;
         }
         Debug.Log("[model instantiate] generate " + index + " th model");
     }
--- Assets/Scripts/IKModelController.cs
+++ Assets/Scripts/IKModelController.cs
@@ -98,7 +98,6 @@ public class IKModelController : MonoBehaviour
 
         private float modelHandDis;
 
-
         void Start()
         {
 
